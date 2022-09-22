62939c0fc925755d6af6d901e8daf022f70c192b
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2022-09-22 09:16:48 +0200

    Update Exercise.cs

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._01
 {
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+            double[] a = { 1, 2, 3, 4, 5 };
+            return a;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers[1];
         }
     }
 }
